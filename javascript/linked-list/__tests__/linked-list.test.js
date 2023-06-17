3f496a10884aff3864020012f885f9a26027c54b
Hamzamt99 <tamarihamza4@gmail.com>
2023-06-17 14:54:11 +0000

    done

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -4,7 +4,37 @@
 const LinkedList = require('../index');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  it('head to be empty',() =>{
+    const ll = new LinkedList();
+    expect(ll.head).toBeNull();
+  }); 
+   it(' append into the linked list',() =>{
+    const ll = new LinkedList();
+    ll.append(30)
+    expect(ll.head.value).toEqual(30);
+    ll.append(10)
+    ll.append(20)
+    expect(ll.head.next.value).toEqual(10);
+  });
+  it('return true when finding a value',() =>{
+    const ll = new LinkedList();
+    ll.append(20)
+    ll.append(30)
+    expect(ll.isInclude(30)).toBe(true);
+  }); 
+  it('return false when not finding a value',() =>{
+    const ll = new LinkedList();
+    ll.append(20)
+    ll.append(30)
+    expect(ll.isInclude(40)).toBe(false);
+  }); 
+  
+  test('should return all values in the list', () => {
+    const ll = new LinkedList();
+    ll.append(10);
+    ll.append(20);
+    ll.append(30);
+    const values = ll.getAllValues();
+    expect(values).toEqual([10, 20, 30]);
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,80 @@
 'use strict';
 
-class LinkedList {
+
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
 
 }
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    //append
+    append(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+          this.head = newNode;
+          this.tail = newNode;
+        } else {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        }
+      }
+
+    // isInclude
+    isInclude(value) {
+        let current = this.head;
+
+        while (current !== null) {
+            if (current.value === value) {
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+    // printAllList
+    getAllValues() {
+        const values = [];
+        let current = this.head;
+    
+        while (current !== null) {
+          values.push(current.value);
+          current = current.next;
+        }
+    
+        return values;
+      }
+   
+    // convert the node to this format node -> node -> null
+    toString() {
+        let current = this.head;
+        let result = '';
+
+        while (current !== null) {
+            result += `{ ${current.value} } -> `;
+            current = current.next;
+        }
+
+        result += 'NULL';
+        return result;
+    }
+
+}
+const ll = new LinkedList();
+ll.append(100)
+ll.append(200)
+ll.append('a')
+ll.getAllValues();
+let test = ll.isInclude('a')
+// console.log(test)
+// console.log(ll.toString())
+
 
 module.exports = LinkedList;
