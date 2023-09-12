64f2734bc9acbcfc98cb6b2e097073c179910b17
Hamzamt99 <tamarihamza4@gmail.com>
2023-09-12 18:52:25 +0000

    done challenge 33

---
+++ javascript/Hash/stringHash/index.js
@@ -0,0 +1,17 @@
+
+function leftJoin(one, two) {
+    let result = [];
+  
+    const keys = one.keys();
+  
+    for (let key of keys) {
+      if (two.has(key)) {
+        result.push([key, one.get(key), two.get(key)]); 
+      }else {
+          result.push([key, one.get(key), null]); 
+  
+      }
+    }
+  
+    return result;
+  }
